3eba4f94d563c7b7a10408e679d62664bc12721e
shradhaagarwal01 <62115289+shradhaagarwal01@users.noreply.github.com>
2021-06-11 19:49:01 +0530

    Updates

---
+++ precache-manifest.f76dbc7997aabae5d442449672b16cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec09d02b5c45835b829da7d895d68ad3",
+    "url": "/Box-Office-App/index.html"
+  },
+  {
+    "revision": "014516133905ac5a8e2b",
+    "url": "/Box-Office-App/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "6880265dd2c9db3a91fa",
+    "url": "/Box-Office-App/static/js/2.5c6a3f27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-Office-App/static/js/2.5c6a3f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014516133905ac5a8e2b",
+    "url": "/Box-Office-App/static/js/main.8ba41e04.chunk.js"
+  },
+  {
+    "revision": "95940e3eb4cdbec258c3",
+    "url": "/Box-Office-App/static/js/runtime-main.0a611d83.js"
+  }
+]);
